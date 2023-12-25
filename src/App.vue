3f3ad2dcedfef06ca7ecde7345963b42d53825ef
Gül Sevim BÜLBÜL <gulsevimblbl@gmail.com>
2023-12-25 22:01:47 +0300

    component creation

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+ 
+ <div id="app">
+
+    <ProductList :products="products"/>
+ </div>
+  
+</template>
+
+<script>
+
+import ProductList from './components/ProductList.vue';
+export default {
+  name: 'App',
+  components: {
+  ProductList
+    
+  },
+  data(){
+    return{
+      products:[
+        {id:1,categoryId:1,productName:"Laptop",quantityPerUnit:'Acer Laptop',unitPrice:5000,unitsInWtock:2},
+        {id:2,categoryId:1,productName:"Mouse",quantityPerUnit:'Acer Mouse',unitPrice:5000,unitsInWtock:3},
+        {id:3,categoryId:2,productName:"Keyboard",quantityPerUnit:'Acer Keyboard',unitPrice:5000,unitsInWtock:20},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+    <h3>Product List</h3>
+    {{ products.length }}
+   </div>
+</template>
+
+<script>
+export default {
+name: "product-list",
+props:{
+    products:Array
+}
+
+};
+</script>
+
+<style scoped>
+
+</style>
