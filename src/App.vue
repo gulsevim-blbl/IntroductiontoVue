9619d6b99938bf67830f2678a78e280d71c18fe9
Gül Sevim BÜLBÜL <gulsevimblbl@gmail.com>
2023-12-26 01:07:46 +0300

    crud demo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
  
  <div id="app">
-
-    <ProductList :products="products"/>
+    <ProductAdd @add:product="addProduct"/>
+    <ProductList @delete:product="deleteProduct" @update:product="updatedProduct" :products="products"/>
  </div>
   
 </template>
@@ -10,21 +10,38 @@
 <script>
 
 import ProductList from './components/ProductList.vue';
+import ProductAdd from './components/ProductAdd.vue';
 export default {
   name: 'App',
   components: {
-  ProductList
+  ProductList,
+  ProductAdd
     
   },
   data(){
     return{
       products:[
-        {id:1,categoryId:1,productName:"Laptop",quantityPerUnit:'Acer Laptop',unitPrice:5000,unitsInWtock:2},
-        {id:2,categoryId:1,productName:"Mouse",quantityPerUnit:'Acer Mouse',unitPrice:5000,unitsInWtock:3},
-        {id:3,categoryId:2,productName:"Keyboard",quantityPerUnit:'Acer Keyboard',unitPrice:5000,unitsInWtock:20},
+        {id:1,categoryId:1,productName:"Laptop",quantityPerUnit:'Acer Laptop',unitPrice:5000,unitsInStock: 2},
+        {id:2,categoryId:1,productName:"Mouse",quantityPerUnit:'Acer Mouse',unitPrice:5000,unitsInStock: 3},
+        {id:3,categoryId:2,productName:"Keyboard",quantityPerUnit:'Acer Keyboard',unitPrice:5000,unitsInStock: 20},
       ]
     }
+  },
+  methods:{
+    deleteProduct(product){
+        this.products =this.products.filter(
+          productToFilter => productToFilter.id!==product.id
+        )
+    },
+    updatedProduct(){
+
+    },
+    addProduct(product){
+      const newProduct = {...product}
+      this.products = [...this.products,newProduct]
+    }
   }
+  
 }
 </script>
 
@@ -33,7 +50,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  
   color: #2c3e50;
   margin-top: 60px;
 }
